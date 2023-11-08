d2e694606c5279b72cee0c645430112dd0836021
zainy123 <zainulabden018@gmail.com>
2023-11-08 22:08:44 +0500

    start

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,30 @@
 // import Container from "../assets/container.jpg";
-import { NavLink }  from 'react-router-dom'
 
+import { FcSearch } from "react-icons/fc";
 
 const Home = () => {
   return (
     <>
-      <div className="flex flex-col pl-12 items-center justify-center bg_img ">
-        <div>
-          <h1 className="text-7xl text-white mb-2">Click Me I Will Surprise You</h1>
+      <div className="flex flex-col pl-12 items-center justify-center bg_img">
+        <div className="mb-4">
+          <h3 className="font-sourcecode font-bold text-3xl text-navyblue">
+            Walpaper Paradise
+          </h3>
         </div>
-        <div className='border rounded-lg p-3 bg-green-500 text-white border-none tooltip'>
-        <NavLink to='/nothing'>Are you sure</NavLink>
+        <div className="text-[#0F2C59]">
+          <h1 className="text-xl mb-2">
+            Best And Stunning Walpapers for your mobile and desktop
+          </h1>
+        </div>
+        <div className="w-[40%] mt-4 font-roboto flex items-center justify-between">
+          <input
+            className="p-3 w-full bg-transparent outline-none border-none rounded-[20px] ring-[10px]"
+            type="text"
+            placeholder="Search Walpaper"
+          />
+          <div className="absolute right-[500px] cursor-pointer">
+            <FcSearch />
+          </div>
         </div>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      navyblue: "#164863",
+      lightskin: "#DDF2FD",
+      white: "#ffffff",
+      midnight: "#121063",
+      metal: "#565584",
+      tahiti: "#3ab7bf",
+      silver: "#ecebff",
+      "bubble-gum": "#ff77e9",
+      bermuda: "#78dcca",
+    },
+    fontFamily: {
+      sans: ["Open Sans", "sans-serif"],
+      roboto: ["Roboto", "sans-serif"],
+      sourcecode: ["Source Code Pro", "monospace"],
+    },
   },
   plugins: [],
 };
