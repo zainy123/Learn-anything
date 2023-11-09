1ca9df970714a857aeb00fbf597f3c737907b10e
zainy123 <zainulabden018@gmail.com>
2023-11-09 21:49:09 +0500

    search

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,33 @@
 // import Container from "../assets/container.jpg";
 // import { FcSearch } from "react-icons/fc";
-import {FiSearch} from 'react-icons/fi'
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { FiSearch } from "react-icons/fi";
 const Home = () => {
+  const [query, setQuery] = useState("");
+  const [data, setData] = useState([]);
+  // const [loading, setLoading] = useState(true);
+
+  const getPhotos = async () => {
+    try {
+      // setLoading(true);
+      const {data} = await axios.get(`https://api.pexels.com/v1/search?query=${query}`, {
+        headers: {
+          Authorization : 'RaeLfBqyyludtfdSd9IVkJ8Nd7juJKb8HESdxL0Z2LvLuRg2kDxgYZGb'
+        }
+      })
+      setData(data.photos)
+    } catch (error) {
+      // setLoading(false);
+      console.log(error);
+    }
+  };
+
+  const onKeyHandler = (e) => {
+    if (e.keyCode === 13) {
+      getPhotos();
+    }
+  };
   return (
     <>
       <div className="flex flex-col items-center text-center justify-center bg_img">
@@ -16,13 +42,31 @@ const Home = () => {
           </h1>
         </div>
         <div className="w-[50%] flex items-center relative text-gray-400 focus-within:text-bermuda">
-          <FiSearch className='absolute w-5 h-5 ml-3'/>
+          <FiSearch className="absolute w-5 h-5 ml-3" />
           <input
             className="pr-3 pl-10 py-2 w-full rounded-2xl border-none outline-none ring-2"
             type="text"
             placeholder="Search Walpaper"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyDown={onKeyHandler}
           />
         </div>
+      </div>
+      {/* {loading && <h1>Fetching ....</h1>} */}
+      <div className="grid sm:grid-cols-2 md:grid-cols-4 justify-center gap-4 items-center mt-[20px] ">
+        {data?.map((item) => {
+          return (
+              <img
+                key={item.id}
+                id={item.id}
+                src={item.src.medium}
+                alt={item.id}
+                className="bg-cover w-[200px] h-[200px] justify-center rounded-lg"
+              />
+            
+          );
+        })}
       </div>
     </>
   );
