641f54f385603eae4ae1a804b072f63fda49500c
zainy123 <zainulabden018@gmail.com>
2023-11-09 15:20:46 +0500

    header

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,27 @@
 // import Container from "../assets/container.jpg";
-
-import { FcSearch } from "react-icons/fc";
-
+// import { FcSearch } from "react-icons/fc";
+import {FiSearch} from 'react-icons/fi'
 const Home = () => {
   return (
     <>
-      <div className="flex flex-col pl-12 items-center justify-center bg_img">
+      <div className="flex flex-col items-center text-center justify-center bg_img">
         <div className="mb-4">
-          <h3 className="font-sourcecode font-bold text-3xl text-navyblue">
+          <h3 className="font-sourcecode font-bold xs:text-[40px] text-navyblue">
             Walpaper Paradise
           </h3>
         </div>
         <div className="text-[#0F2C59]">
-          <h1 className="text-xl mb-2">
+          <h1 className="text-[30px] mb-2">
             Best And Stunning Walpapers for your mobile and desktop
           </h1>
         </div>
-        <div className="w-[40%] mt-4 font-roboto flex items-center justify-between">
+        <div className="xs:w-[40%] flex items-center relative text-gray-400 focus-within:text-bermuda">
+          <FiSearch className='absolute w-5 h-5 ml-3'/>
           <input
-            className="p-3 w-full bg-transparent outline-none border-none rounded-[20px] ring-[10px]"
+            className="pr-3 pl-10 py-2 w-full rounded-2xl border-none outline-none ring-2"
             type="text"
             placeholder="Search Walpaper"
           />
-          <div className="absolute right-[500px] cursor-pointer">
-            <FcSearch />
-          </div>
         </div>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '295px',
+        'sm': '360px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl':'1280px',
+        '2xl': '1536px'
+      }
+    },
     colors: {
       navyblue: "#164863",
       lightskin: "#DDF2FD",
